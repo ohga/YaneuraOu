e4b445e1c947ccba55c9a055096b450a3474622c
ebifrier <ebifrier@gmail.com>
2018-04-04 14:55:58 +0900

    末端クライアントのバージョンを更新

--- source/godwhale/version.hpp
+++ source/godwhale/version.hpp
@@ -2,7 +2,7 @@
 #ifndef GODWHALE_CHILD_VERSION_HPP
 #define GODWHALE_CHILD_VERSION_HPP
 
-#define SlaveName "GodwhaleChild-5.0.2"
+#define SlaveName "GodwhaleChild-5.0.3"
 
 constexpr int LoginNameMaxLength = 12;
 constexpr uint64_t EvalCheckSum = 0x0000cdfb3ca11fcULL;
