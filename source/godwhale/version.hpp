4f9ca266711007dfff8ace6746702a56590be3f9
ebifrier <ebifrier@gmail.com>
2018-05-02 11:50:57 +0900

    Update version.hpp

--- source/godwhale/version.hpp
+++ source/godwhale/version.hpp
@@ -2,7 +2,7 @@
 #ifndef GODWHALE_CHILD_VERSION_HPP
 #define GODWHALE_CHILD_VERSION_HPP
 
-#define SlaveName "GodwhaleChild-5.0.4"
+#define SlaveName "GodwhaleChild-5.0."
 
 constexpr int LoginNameMaxLength = 12;
 constexpr uint64_t EvalCheckSum = 0x00000d4a2143da16ULL;
